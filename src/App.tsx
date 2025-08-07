3bfac46476e07c0076a18c987354269fba7b7d50
Cameron Harp <info@harpgmg.com>
2025-08-07 16:49:51 -0600

    updated the more dropdown and move staff to the top

--- src/App.tsx
+++ src/App.tsx
@@ -47,11 +47,11 @@ function MoreDropdown({ isMobile = false, onItemClick = () => {} }) {
   };
 
   const items = [
+    { label: 'Our Staff', url: '/staff' },
     { label: 'Pricing', url: '/pricing' },
     { label: 'Pro Shop', url: 'https://rmtwear.com/' },
     { label: 'Holiday Hours', url: '/holiday-hours' },
     { label: 'Parent Volunteers', url: '/parent-volunteers' },
-    { label: 'Our Staff', url: '/staff' },
     { label: 'FAQ', url: '/faq' },
     { label: 'Open Gym', url: '/open-gym' },
     { label: 'Preschool', url: 'https://www.rockymountainlearningacademy.com/' },
