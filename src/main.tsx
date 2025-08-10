d5befd1b328dd94ccbd142b142a8c354afa827e3
Cameron Harp <info@harpgmg.com>
2025-08-10 13:06:16 -0600

    fixed 404 issue when refreshing on sub-page

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,19 @@ import { BrowserRouter, useLocation } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
+// GitHub Pages SPA support
+function handleGitHubPagesSPA() {
+  const search = window.location.search;
+  if (search) {
+    const query = new URLSearchParams(search);
+    const redirect = query.get('redirect') || query.get('/');
+    if (redirect) {
+      const newUrl = redirect.replace(/~and~/g, '&');
+      window.history.replaceState(null, '', newUrl);
+    }
+  }
+}
+
 function ScrollToTop() {
   const location = useLocation();
   
@@ -20,6 +33,9 @@ function ScrollToTop() {
 
   return null;
 }
+
+// Handle GitHub Pages SPA routing before React renders
+handleGitHubPagesSPA();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
